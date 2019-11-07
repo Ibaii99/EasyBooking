f03db5e7c8761e47f4f5225d49d7d5a512bb56cc
Ibaii99 <ibai.guillen@opendeusto.es>
2019-11-07 09:14:22 +0100

    Minimal changes

--- src/bin/bd/BD.java
+++ src/bin/bd/BD.java
@@ -4,6 +4,7 @@ import java.io.IOException;
 import java.util.logging.ConsoleHandler;
 import java.util.logging.Logger;
 
+
 public class BD {
 	
 	public String location = "src/bin/bd/EasyBooking.db"; 
