f4a3ffd6433506a7610e7ab611a44a22ed069575
Jon Uraga <j.uraga@opendeusto.es>
2019-11-07 17:14:20 +0100

    Main con insert y select sin terminar

--- src/bin/main/Main.java
+++ src/bin/main/Main.java
@@ -14,20 +14,50 @@ public class Main {
 		try {
 			PersistenceManagerFactory persistentManagerFactory = JDOHelper.getPersistenceManagerFactory("datanucleus.properties");
 			
+			//Insert data in the DB
 			PersistenceManager persistentManager = persistentManagerFactory.getPersistenceManager();				
 			Transaction transaction = persistentManager.currentTransaction();	
 			try {
 				transaction.begin();
 				// 	TODO 
+				
+				
+				
 				System.out.println("- Inserted into db: ");
 				transaction.commit();
 			} catch (Exception e) {
 				System.err.println("* Exception inserting data into db: " + e.getMessage());
-			} 
+			} finally {
+				if (transaction.isActive()) {
+					transaction.rollback();
+				}
+				persistentManager.close();
+			}
+			
+			//Select data using a Query
+			persistentManager = persistentManagerFactory.getPersistenceManager();
+			transaction = persistentManager.currentTransaction();
+			
+			try {
+				transaction.begin();
+				// TODO 
+				
+				
+				
+				
+				
+				transaction.commit();
+			} catch (Exception e) {
+				System.err.println("* Exception executing a query: " + e.getMessage());
+			} finally {
+				if (transaction.isActive()) {
+			        transaction.rollback();
+			    }
+	
+			    persistentManager.close();
+			}
 		} catch (Exception e) {
-			// TODO: handle exception
+			System.err.println("* Exception: " + e.getMessage());
 		}
-
 	}
-
 }
