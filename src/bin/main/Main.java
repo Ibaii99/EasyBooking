6b829e70eb48eac420e01e879da37a1d78689b70
Jon Uraga <j.uraga@opendeusto.es>
2019-11-08 12:26:37 +0100

    Más objetos creados en el Main, da errores pero compila

--- src/bin/main/Main.java
+++ src/bin/main/Main.java
@@ -7,6 +7,8 @@ import javax.jdo.PersistenceManagerFactory;
 import javax.jdo.Query;
 import javax.jdo.Transaction;
 
+import entidades.pago.Pago;
+import entidades.reserva.Reserva;
 import entidades.usuario.Usuario;
 
 
@@ -14,7 +16,18 @@ public class Main {
 
 	public static void main(String[] args) {
 		
-		Usuario u = new Usuario("ibai", "asidhaiubuad@jasnka.es", 20, "BIO");
+		Usuario u1 = new Usuario("ibai", "asidhaiubuad@jasnka.es", 20, "BIO");
+		Usuario u2 = new Usuario("Jon", "jjj@jojo.com", 19, "BIO");
+		Usuario u3 = new Usuario("Gorka", "gor@do.com", 20, "MAD");
+		
+		Reserva r1 = new Reserva("tarjeta", 2, "8-11-2019");
+		Reserva r2 = new Reserva("paypal", 3, "7-11-2019");
+		byte tarjeta[] = {0,0,0,0,1,1,1,1,2,2,2,2,3,3,3,5};
+		Pago p1 = new Pago(u1.getEmail(), 149.98);
+		Pago p2 = new Pago("",tarjeta, "", 231.97);
+		
+		u1.getReservas().add(r1);
+		u2.getReservas().add(r2);
 		
 		
 		try {
@@ -27,7 +40,9 @@ public class Main {
 			try {
 				tx.begin();
 				// 	TODO 
-				pm.makePersistent(u);
+				pm.makePersistent(u1);
+				pm.makePersistent(u2);
+				pm.makePersistent(u3);
 				
 				
 				System.out.println("- Inserted into db: ");
