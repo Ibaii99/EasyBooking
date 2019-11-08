d9e76cc18a30aceae5b5393a3a03d7ec87210894
yeray <yeraybellanco@opendeusto.es>
2019-11-08 20:40:15 +0100

    Mas metodos main

--- src/bin/main/Main.java
+++ src/bin/main/Main.java
@@ -1,5 +1,7 @@
 package bin.main;
 
+import java.util.List;
+
 import javax.jdo.Extent;
 import javax.jdo.JDOHelper;
 import javax.jdo.PersistenceManager;
@@ -19,6 +21,9 @@ public class Main {
 		Usuario u1 = new Usuario("ibai", "asidhaiubuad@jasnka.es", 20, "BIO");
 		Usuario u2 = new Usuario("Jon", "jjj@jojo.com", 19, "BIO");
 		Usuario u3 = new Usuario("Gorka", "gor@do.com", 20, "MAD");
+		Usuario u4 = new Usuario("Yeray","yeray@elcrack.com",20,"BCN");
+		
+		
 		
 		Reserva r1 = new Reserva("tarjeta", 2, "8-11-2019");
 		Reserva r2 = new Reserva("paypal", 3, "7-11-2019");
@@ -26,9 +31,9 @@ public class Main {
 		Pago p1 = new Pago(u1.getEmail(), 149.98);
 		Pago p2 = new Pago("VISA","0000 1111 2222 3333", "06/22", 231.97);
 		
-		u1.getReservas().add(r1);
-		u2.getReservas().add(r2);
-		
+
+		u1.addReserva(r1);
+		u2.addReserva(r2);
 		
 		try {
 			System.out.println("Entra en el try");
@@ -43,6 +48,7 @@ public class Main {
 				pm.makePersistent(u1);
 				pm.makePersistent(u2);
 				pm.makePersistent(u3);
+				pm.makePersistent(u4);
 				
 				
 				System.out.println("- Inserted into db: ");
@@ -62,14 +68,27 @@ public class Main {
 			tx = pm.currentTransaction();
 			
 			try {
+				System.out.println("- Usuarios con aeropuerto en BIO");			
+				//Get the Persistence Manager
+				pm = pmf.getPersistenceManager();
+				//Obtain the current transaction
+				tx = pm.currentTransaction();		
+				//Start the transaction
 				tx.begin();
-				// TODO 
+
+				Query<Usuario> query = pm.newQuery(Usuario.class);
+				query.setFilter("aeropuerto_preferido == 'BIO'" );
 				
+				@SuppressWarnings("unchecked")
+				List<Usuario> usuarios = (List<Usuario>) query.execute();
 				
-				
-				
-				
+				for (Usuario usuario : usuarios) {
+					System.out.println("  -> " + usuario.getNombre());
+				}
+
+				//End the transaction
 				tx.commit();
+					
 			} catch (Exception e) {
 				System.err.println("* Exception executing a query: " + e.getMessage());
 			} finally {
@@ -79,6 +98,41 @@ public class Main {
 	
 			    pm.close();
 			}
+			
+			
+			try {
+				System.out.println("- DELETE - ");			
+				//Get the Persistence Manager
+				pm = pmf.getPersistenceManager();
+				//Obtain the current transaction
+				tx = pm.currentTransaction();		
+				//Start the transaction
+				tx.begin();
+
+				Query<Usuario> usuarios = pm.newQuery(Usuario.class);
+				usuarios.deletePersistentAll();
+				
+				Query<Reserva> reservas = pm.newQuery(Reserva.class);
+				reservas.deletePersistentAll();
+				
+				Query<Pago> pagos = pm.newQuery(Pago.class);
+				usuarios.deletePersistentAll();
+
+				//End the transaction
+				tx.commit();
+					
+			} catch (Exception e) {
+				System.err.println("* Exception executing a query: " + e.getMessage());
+			} finally {
+				if (tx.isActive()) {
+			        tx.rollback();
+			    }
+	
+			    pm.close();
+			}
+			
+			
+			
 		} catch (Exception e) {
 			System.err.println("* Exception: " + e.getMessage());
 		}
