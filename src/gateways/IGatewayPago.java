f5d9d893cb99d262c50815969cf581a8c29ee289
Jon Uraga <j.uraga@opendeusto.es>
2020-01-17 13:14:07 +0100

    Paypal Gateway y service

--- src/gateways/IGatewayPago.java
+++ src/gateways/IGatewayPago.java
@@ -1,5 +1,7 @@
 package gateways;
 
+import java.rmi.RemoteException;
+
 import data.dto.UsuarioDTO;
 import data.dto.VueloDTO;
 
@@ -10,6 +12,6 @@ public interface IGatewayPago {
 	 * @param email Usuairo que paga el vuelo
 	 * @return True si se realiza el pago, False si no
 	 */
-	public boolean realizarPago(int precio, String email);
+	public boolean realizarPago(int precio, String email, String contrasenya) throws RemoteException;
 	
 }
