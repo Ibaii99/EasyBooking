2d3aeb79839c8f99685959b529914004ae1e2c76
yeray <yeraybellanco@opendeusto.es>
2020-01-18 21:26:02 +0100

    Gateway ryanair RMI

--- src/gateways/aerolinea/RyanAirGateway.java
+++ src/gateways/aerolinea/RyanAirGateway.java
@@ -1,43 +1,62 @@
 package gateways.aerolinea;
 
+import java.rmi.RemoteException;
 import java.util.ArrayList;
 
+import Ryanair.IAerolineaRyanair;
+import Ryanair.VueloRyanair;
 import data.dto.VueloDTO;
 import gateways.IGatewayAerolinea;
 
 public class RyanAirGateway implements IGatewayAerolinea{
 	static String IP = "127.0.0.1";
-	static String Puerto = "";
-	static String Service = "";
+	static String Puerto = "1099";
+	static String Service = "Aerolinea_Ryanair";
+	private IAerolineaRyanair server;
 	
-	@Override
-	public ArrayList<VueloDTO> getAllVuelos() {
-		// TODO Auto-generated method stub
-		return null;
-	}
-
-	public VueloDTO buscarVuelo(String aeropuertoDestino, String aeropuertoOrigen, String fechaIda, String fechaVuelta,
-			int asientos) {
-		// TODO Auto-generated method stub
-		return null;
-	}
+	 public RyanAirGateway() {
+		// TODO Auto-generated constructor stub
+		try {
+			server = (IAerolineaRyanair) java.rmi.Naming.lookup(Service);
+		} catch (Exception e) {
+			System.err.println("- Exception running the server: " + e.getMessage());
+		}
+}
 
 	@Override
-	public ArrayList<VueloDTO> buscarVuelosDesdeOrigen(String aeropuertoOrigen, String fecha, int asientos) {
+	public ArrayList<VueloRyanair> getAllVuelos() throws RemoteException {
+		server.getAllVuelos();
 		// TODO Auto-generated method stub
 		return null;
 	}
 
 	@Override
-	public boolean reservarVuelo(VueloDTO vuelo, String nombre, int plazas) {
+	public VueloRyanair buscarVuelo(String aeropuertoDestino, String aeropuertoOrigen, String fecha, int asientos)
+			throws RemoteException {
+		
 		// TODO Auto-generated method stub
-		return false;
+		return server.buscarVuelo(aeropuertoDestino, aeropuertoOrigen, fecha, asientos);
 	}
 
 	@Override
-	public VueloDTO buscarVuelo(String aeropuertoDestino, String aeropuertoOrigen, String fecha, int asientos) {
+	public ArrayList<VueloRyanair> buscarVuelosDesdeOrigen(String aeropuertoOrigen, String fecha, int asientos)
+			throws RemoteException {
+		
 		// TODO Auto-generated method stub
-		return null;
+		return server.buscarVuelosDesdeOrigen(aeropuertoOrigen, fecha, asientos);
 	}
+
+	@Override
+	public boolean reservarVuelo(VueloRyanair vuelo, String nombre, int plazas) throws RemoteException {
+		
+		// TODO Auto-generated method stub
+		return server.reservarVuelo(vuelo, nombre, plazas);
+	}
+	
+	 
+	
+	
+	
+
 
 }
